5394ed1eef08d7b05602eac13fccf5b637a2e079
vananh1309 <anhvv13@gmail.com>
2019-08-09 17:01:59 +0700

    dsdsa

---
+++ Store.Data/Repositories/GadgetRepository.cs
@@ -0,0 +1,22 @@
+﻿using Store.Data.Infrastructure;
+using Store.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data.Repositories
+{
+
+    public interface IGadgetRepository : IRepository<Gadget>
+    {
+
+    }
+
+    public class GadgetRepository : RepositoryBase<Gadget>, IGadgetRepository
+    {
+        public GadgetRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+    }
+}
---
+++ Store.Data/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Store.Data.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private StoreEntities dbContext;
+
+        public UnitOfWork(IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+
+        public StoreEntities DbContext
+        {
+            get { return dbContext ?? (dbContext = dbFactory.Init()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.Commit();
+        }
+    }
+}
